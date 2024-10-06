d8b981cf4d37975a727c0103fdad25f0f627af95
saudahmedkadapa <kadapasaud@gmail.com>
2024-10-06 17:07:22 +0530

    Java

---
+++ ArrayDeclareDt.java
@@ -0,0 +1,34 @@
+public class ArrayDeclareDt {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        for(int i=0;i<numbers.length;i++)
+        {
+            System.out.println(numbers[i]);
+        }
+
+
+        double[] decimals = {1.1, 2.2, 3.3, 4.4, 5.5};
+        for(int i=0;i<decimals.length;i++)
+        {
+            System.out.println(decimals[i]);
+        }
+
+        char[] letters = {'A', 'B', 'C', 'D', 'E'};
+        for(int i=0;i<letters.length;i++)
+        {
+            System.out.println(letters[i]);
+        }
+
+        String[] names = {"Virat", "Rohit", "kane", "Watson"};
+        for(int i=0;i<names.length;i++)
+        {
+            System.out.println(names[i]);
+        }
+
+
+
+        
+        
+    }
+    
+}
