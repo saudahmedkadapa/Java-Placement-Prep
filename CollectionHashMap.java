9417a6c14e3c53eb0152f2acb378b48b49143597
saudahmedkadapa <kadapasaud@gmail.com>
2024-10-08 01:22:03 +0530

    Java

---
+++ CollectionHashMap.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CollectionHashMap {
+    public static void main(String[] args) {
+       HashMap<Integer,String> obj=new HashMap<>();
+       obj.put(56, "Saud");
+       obj.put(8, "ahmed");
+       obj.put(56, "Saud");
+       
+       System.out.println(obj);
+    }
+    
+}
