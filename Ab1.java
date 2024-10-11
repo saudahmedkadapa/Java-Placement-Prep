2a120351a2675eb835b91a8382b3e43991c9246d
saudahmedkadapa <kadapasaud@gmail.com>
2024-10-11 16:45:59 +0530

    java

---
+++ Ab1.java
@@ -0,0 +1,19 @@
+abstract class Ab1 {
+    public abstract void hello();  //abstract method
+
+    public void Game()
+    {
+        System.out.println("Hello Game");
+    }
+
+
+}
+
+
+
+
+
+
+
+    
+
